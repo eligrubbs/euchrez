4b092b09a60c19d93b89f25870d41fa3cc24076c
eligrubbs <gitcontribs@eligrubbs.com>
2025-01-20 12:54:19 -0600

    added stubs with wrong return types.

---
+++ src/game.zig
@@ -0,0 +1,27 @@
+// The `Game` struct contains logic and exposes API's to run a game of euchre.  
+// While Euchre is a 4 player game, the API exposes a simple `step` function.
+
+const Deck = @import("deck.zig").Deck;
+
+const Game = struct {
+    const num_players = 4; // do not change
+
+    deck: Deck,
+
+    /// Creates a game object. It is NOT ready to be played.
+    /// 
+    /// Caller responsible for freeing used memory using `deinit` method
+    pub fn init() void {
+
+    }
+
+    /// Resets the game state to a valid beginning state.
+    pub fn reset() void {
+
+    }
+
+    /// Cleans up game object.
+    pub fn deinit() void {
+
+    }
+};
