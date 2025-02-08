0264319c86b045b223f138304d0041b9aeb296cc
eligrubbs <gitcontribs@eligrubbs.com>
2025-02-07 21:17:08 -0600

    remove default optimization in build.zig.

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
-    const optimize = b.standardOptimizeOption(.{.preferred_optimize_mode = .ReleaseFast});
+    const optimize = b.standardOptimizeOption(.{});
 
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
